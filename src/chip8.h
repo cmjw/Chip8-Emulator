f8ee3c83cfe55f332a6dc29449d1b6f3d73e58eb
Charlotte <cmjwfeltwell@gmail.com>
2024-11-28 12:12:30 -0600

    Add memory, registers, and index register

--- src/chip8.h
+++ src/chip8.h
@@ -1,6 +1,7 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 
+#include <cstdint>
 #include <SDL2/SDL.h>
 
 const int DISPLAY_WIDTH = 64;
@@ -11,6 +12,19 @@ class Chip8 {
 public:
     Chip8();
     ~Chip8();
+
+    uint8_t memory[4096]; // 4096 bytes of memory
+    /* - 0x200 to 0xFFF is program space
+       - 0x000 to 0x1FF is reserved
+           - 0x050 to 0x0A0 stores 16 built-in chars */
+
+    uint8_t registers[16]; // 16 8-bit registers
+    /* V0 to VF, 0x00 to 0xFF
+        VF is used as a flag */
+
+    uint16_t index; // special 16-bit index register
+    /* stores an address. needed because 8-bit registers can't store
+        largest location. */
 
     void LoadROM(const char* filename);
 
