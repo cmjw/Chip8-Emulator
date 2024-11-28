087adf4f6578ea41609b33ceee8569f219c86c98
Charlotte <cmjwfeltwell@gmail.com>
2024-11-28 11:35:05 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "chip8.h"
 
 int main(int argc, char* argv[]) {
-    Chip8 emulator;
-    emulator.Run();
+    Chip8 chip8;
+    const char* ROMfilename = "../roms/test.ch8";
+
+    chip8.LoadROM(ROMfilename);
     return 0;
 }
