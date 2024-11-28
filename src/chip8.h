19cf8d2e5b11b9f55297506f63eea7400b0a8e88
Charlotte <cmjwfeltwell@gmail.com>
2024-11-27 21:27:47 -0600

    Create chip8.h

---
+++ src/chip8.h
@@ -0,0 +1,25 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+
+#include <SDL2/SDL.h>
+
+const int DISPLAY_WIDTH = 64;
+const int DISPLAY_HEIGHT = 32;
+const int PIXEL_SCALE = 10; 
+
+class Chip8 {
+public:
+    Chip8();
+    ~Chip8();
+
+    void Run();         
+    void Render();
+    void HandleInput();
+
+private:
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    bool running;
+};
+
+#endif
