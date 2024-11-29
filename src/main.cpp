0deb1032db2d13c02280672711cc1cf313086b09
Charlotte <cmjwfeltwell@gmail.com>
2024-11-29 15:56:44 -0600

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
 #include "chip8.h"
 #include "chip8video.h"
+#include <iostream>
+#include <string>
 
 int main(int argc, char* argv[]) {
     // print signs of life
     printf("||||||||||||||||\n");
-    printf("Chip8 Emulator\n");
+    printf(" Chip8 Emulator\n");
     printf("||||||||||||||||\n\n");
 
-    Chip8 chip8;
-    const char* ROMfilename = "../roms/test.ch8";
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <Scale> <Delay> <ROM>\n";
+        return -1;
+    }
 
+    // cmd args
+    int videoScale = std::stoi(argv[1]);
+    int cycleDelay = std::stoi(argv[2]);
+    char const* ROMfilename = argv[3];
+
+    Chip8 chip8;
     chip8.LoadROM(ROMfilename);
 
     chip8.MemoryDump();
