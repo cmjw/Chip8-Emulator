8edff5710ae431670d6c16ce27bb9b5175b888ba
Charlotte <cmjwfeltwell@gmail.com>
2024-11-28 19:52:09 -0600

    Add signs of life

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include "chip8.h"
 
 int main(int argc, char* argv[]) {
+    // print signs of life
+    printf("||||||||||||||||\n");
+    printf("Chip8 Emulator\n");
+    printf("||||||||||||||||\n");
+
     Chip8 chip8;
     const char* ROMfilename = "../roms/test.ch8";
 
     chip8.LoadROM(ROMfilename);
+
+    printf("Quit\n");
     return 0;
 }
