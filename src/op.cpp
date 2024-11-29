cac2f16e0a5be32d6d76a49ae50ab149d8f6f51b
Charlotte <cmjwfeltwell@gmail.com>
2024-11-28 19:44:53 -0600

    00EE RET

--- src/op.cpp
+++ src/op.cpp
@@ -1,10 +1,19 @@
 #include "chip8.h"
 
-/*
+/**
  * CLS (0x00E0) 
  * Clear the display.
  */
 void Chip8::OP_00E0() {
     // set video buffer to zeroes
     memset(video, 0, sizeof(video));
+}
+
+/**
+ * RET (0x00EE)
+ * Return from a subroutine/function.
+ */
+void Chip8::OP_00EE() {
+    sp--;
+    pc = stack[pc];
 }
