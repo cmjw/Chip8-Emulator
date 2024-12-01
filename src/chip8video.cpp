e7a757ff3ea2322d8b44cf9507449f381e360b46
Charlotte <cmjwfeltwell@gmail.com>
2024-11-30 19:29:17 -0600

    Implement HandleInput()

--- src/chip8video.cpp
+++ src/chip8video.cpp
@@ -27,3 +27,160 @@ void Chip8_Video::Update(const void* buffer, int pitch) {
     SDL_RenderCopy(renderer, texture, nullptr, nullptr);
     SDL_RenderPresent(renderer);
 }
+
+/**
+ * Handle keypad input.
+ */
+bool Chip8_Video::HandleInput(uint8_t* keypad) {
+    bool quit = false;
+    SDL_Event event;
+
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+			case SDL_QUIT: {
+				quit = true;
+			} break;
+
+			case SDL_KEYDOWN: {
+				switch (event.key.keysym.sym) {
+					case SDLK_ESCAPE: {
+						quit = true;
+					} break;
+
+					case SDLK_x: {
+						keypad[0] = 1;
+					} break;
+
+					case SDLK_1: {
+						keypad[1] = 1;
+					} break;
+
+					case SDLK_2: {
+						keypad[2] = 1;
+					} break;
+
+					case SDLK_3: {
+						keypad[3] = 1;
+					} break;
+
+					case SDLK_q: {
+						keypad[4] = 1;
+					} break;
+
+					case SDLK_w: {
+						keypad[5] = 1;
+					} break;
+
+					case SDLK_e: {
+						keypad[6] = 1;
+					} break;
+
+					case SDLK_a: {
+						keypad[7] = 1;
+					} break;
+
+					case SDLK_s: {
+						keypad[8] = 1;
+					} break;
+
+					case SDLK_d: {
+						keypad[9] = 1;
+					} break;
+
+					case SDLK_z: {
+						keypad[0xA] = 1;
+					} break;
+
+					case SDLK_c: {
+						keypad[0xB] = 1;
+					} break;
+
+					case SDLK_4: {
+						keypad[0xC] = 1;
+					} break;
+
+					case SDLK_r: {
+						keypad[0xD] = 1;
+					} break;
+
+					case SDLK_f: {
+						keypad[0xE] = 1;
+					} break;
+
+					case SDLK_v: {
+					    keypad[0xF] = 1;
+					} break;
+				}
+			} break;
+
+			case SDL_KEYUP: {
+				switch (event.key.keysym.sym) {
+					case SDLK_x: {
+					    keypad[0] = 0;
+					} break;
+
+					case SDLK_1: {
+						keypad[1] = 0;
+					} break;
+
+					case SDLK_2: {
+						keypad[2] = 0;
+					} break;
+
+					case SDLK_3: {
+						keypad[3] = 0;
+					} break;
+
+					case SDLK_q: {
+						keypad[4] = 0;
+					} break;
+
+					case SDLK_w: {
+						keypad[5] = 0;
+					} break;
+
+					case SDLK_e: {
+						keypad[6] = 0;
+					} break;
+
+					case SDLK_a: {
+						keypad[7] = 0;
+					} break;
+
+					case SDLK_s: {
+						keypad[8] = 0;
+					} break;
+
+					case SDLK_d: {
+						keypad[9] = 0;
+					} break;
+
+					case SDLK_z: {
+						keypad[0xA] = 0;
+					} break;
+
+					case SDLK_c: {
+						keypad[0xB] = 0;
+					} break;
+
+					case SDLK_4: {
+						keypad[0xC] = 0;
+					} break;
+
+					case SDLK_r: {
+						keypad[0xD] = 0;
+					} break;
+
+					case SDLK_f: {
+						keypad[0xE] = 0;
+					} break;
+
+					case SDLK_v: {
+					    keypad[0xF] = 0;
+					} break;
+				}
+			} break;
+		}
+	}
+    return quit;
+}
