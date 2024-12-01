aea46ed44b45b6b1b4cf2110f80bf3a924b2e71e
Charlotte <cmjwfeltwell@gmail.com>
2024-11-30 18:36:15 -0600

    Update constants

--- src/chip8.h
+++ src/chip8.h
@@ -7,6 +7,9 @@
 #include <chrono>
 #include <cstring>
 
+const unsigned int MEMORY_SIZE = 4096;
+const unsigned int RESERVED_MEMORY_SIZE = 512;
+
 const unsigned int START_ADDRESS = 0x200;
 const unsigned int FONTSET_START_ADDRESS = 0x50;
 
@@ -63,7 +66,7 @@ public:
     // void HandleInput();
 
 private:
-    uint8_t memory[4096]; // 4096 bytes of memory
+    uint8_t memory[MEMORY_SIZE]; // 4096 bytes of memory
     /* - 0x200 to 0xFFF is program space
        - 0x000 to 0x1FF is reserved
            - 0x050 to 0x0A0 stores 16 built-in chars */
