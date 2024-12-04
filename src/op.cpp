b65fba0866dcb4d022323cd6f4fa12465dca4b1b
Charlotte <cmjwfeltwell@gmail.com>
2024-12-03 18:44:15 -0600

    Change Vx -> x

--- src/op.cpp
+++ src/op.cpp
@@ -400,24 +400,24 @@ void Chip8::OP_Fx07() {
  * Store the key value in Vx.
  */
 void Chip8::OP_Fx0A() {
-    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t x = (opcode & 0x0F00u) >> 8u;
 
-	if (keypad[0]) { registers[Vx] = 0; }
-	else if (keypad[1]) { registers[Vx] = 1; }
-	else if (keypad[2]) { registers[Vx] = 2; }
-	else if (keypad[3]) { registers[Vx] = 3; }
-	else if (keypad[4]) { registers[Vx] = 4; }
-	else if (keypad[5]) { registers[Vx] = 5; }
-	else if (keypad[6]) { registers[Vx] = 6; }
-	else if (keypad[7]) { registers[Vx] = 7; }
-	else if (keypad[8]) { registers[Vx] = 8; }
-	else if (keypad[9]) { registers[Vx] = 9; }
-	else if (keypad[10]) { registers[Vx] = 10; }
-	else if (keypad[11]) { registers[Vx] = 11; }
-	else if (keypad[12]) { registers[Vx] = 12; }
-	else if (keypad[13]) { registers[Vx] = 13; }
-	else if (keypad[14]) { registers[Vx] = 14; }
-	else if (keypad[15]) { registers[Vx] = 15; }
+	if (keypad[0]) { registers[x] = 0; }
+	else if (keypad[1])  { registers[x] = 1; }
+	else if (keypad[2])  { registers[x] = 2; }
+	else if (keypad[3])  { registers[x] = 3; }
+	else if (keypad[4])  { registers[x] = 4; }
+	else if (keypad[5])  { registers[x] = 5; }
+	else if (keypad[6])  { registers[x] = 6; }
+	else if (keypad[7])  { registers[x] = 7; }
+	else if (keypad[8])  { registers[x] = 8; }
+	else if (keypad[9])  { registers[x] = 9; }
+	else if (keypad[10]) { registers[x] = 10; }
+	else if (keypad[11]) { registers[x] = 11; }
+	else if (keypad[12]) { registers[x] = 12; }
+	else if (keypad[13]) { registers[x] = 13; }
+	else if (keypad[14]) { registers[x] = 14; }
+	else if (keypad[15]) { registers[x] = 15; }
 	else {
 		pc -= 2;
 	}
