ee81303f02d2684ab622e2ecab8a810f7d999d94
Charlotte <cmjwfeltwell@gmail.com>
2024-12-04 13:16:31 -0600

    Update main.cpp

--- disassemble/main.cpp
+++ disassemble/main.cpp
@@ -1,5 +1,11 @@
 #include "disassembler.h"
+#include <stdio.h>
 
-int main () {
+int main (int argc, char* argv[]) {
+    // check args
+    if (argc < 2) {
+        printf("Usage: %s <file to disassemble\n", argv[0]);
+    }
+
     return 0;
 }
